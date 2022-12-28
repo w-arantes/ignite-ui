3ce2cf4df237d1189524831661cfac783563b6d8
Wellington Arantes <w.aranteslfc@gmail.com>
2022-12-28 00:55:26 -0300

    feat(components): avatar component

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data Display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/facebook.png',
+    alt: 'Meta Logo',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'This feature allow to specify a fallback avatar image if they cannot be found on the original host.',
+      },
+    },
+  },
+}
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,4 @@
+export * from './components/Avatar'
 export * from './components/Box'
-export * from './components/Text'
 export * from './components/Heading'
+export * from './components/Text'
